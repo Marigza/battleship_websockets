1a720eb5864d4e1baa3a773b599619863e403ac5
Marigza <marigola@mail.ru>
2024-02-26 09:31:15 +0300

    refactor: linter fixing

--- src/battle_ship/models.ts
+++ src/battle_ship/models.ts
@@ -1,60 +1,59 @@
-import WebSocket from "ws";
+import WebSocket from 'ws';
 
 export interface CommandData {
-  type: string,
-  data: string,
-  id: number,
+  type: string;
+  data: string;
+  id: number;
 }
 
-export interface CustomSocket extends WebSocket  {
-  id: number
+export interface CustomSocket extends WebSocket {
+  id: number;
 }
 
 export interface UserRegistration {
-  name: string,
-  index: number,
-  error: boolean,
-  errorText: string
+  name: string;
+  index: number;
+  error: boolean;
+  errorText: string;
 }
 
 export interface UserData {
-  name: string,
-  password: string
+  name: string;
+  password: string;
 }
 
 export interface Winner {
-  name: string,
-  wins: number
+  name: string;
+  wins: number;
 }
 
-
 export interface CurrentSessionUser {
-  indexUser: number,
-  userData: string
+  indexUser: number;
+  userData: string;
 }
 
 export interface Ship {
   position: {
-      x: number,
-      y: number,
-  },
-  direction: boolean,
-  length: number,
-  type: "small"|"medium"|"large"|"huge",
+    x: number;
+    y: number;
+  };
+  direction: boolean;
+  length: number;
+  type: 'small' | 'medium' | 'large' | 'huge';
 }
 
 export interface Attack {
-  gameId: number,
-  x?: number,
-  y?: number,
-  indexPlayer: number,
+  gameId: number;
+  x?: number;
+  y?: number;
+  indexPlayer: number;
 }
 
 export interface ShipInBattle {
-  position: string[],
-  length: number,
-  type: "small"|"medium"|"large"|"huge",
-  isKill: boolean,
-  isShot: boolean,
-  aliveLength: number
+  position: string[];
+  length: number;
+  type: 'small' | 'medium' | 'large' | 'huge';
+  isKill: boolean;
+  isShot: boolean;
+  aliveLength: number;
 }
--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -7,41 +7,42 @@ import { CommandData, CustomSocket } from '../battle_ship/models';
 import { SocketArray, switchCommand } from '../battle_ship/switchCommand';
 
 export const httpServer = http.createServer(function (req, res) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
+  const __dirname = path.resolve(path.dirname(''));
+  const file_path =
+    __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+  fs.readFile(file_path, function (err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
 });
 
 export const wss = new WebSocketServer({ port: 3000 });
 
-let dataFromUser : CommandData;
+let dataFromUser: CommandData;
 let socketCounter = 1;
 
 wss.on('connection', (ws: CustomSocket) => {
-    console.log('connection ready');
-    
-    ws.id = socketCounter++;
+  console.log('connection ready');
 
-    SocketArray.push(ws)
+  ws.id = socketCounter++;
 
-    ws.on('error', console.error);
- 
-    ws.on('message', function message(data) {
-        const dataToString = data.toString();
-        dataFromUser = JSON.parse(dataToString);
+  SocketArray.push(ws);
 
-        switchCommand(dataFromUser, ws);
-    });
+  ws.on('error', console.error);
 
-    ws.on('close', function close() {
-        console.log('disconnected');
-    })
-})
+  ws.on('message', function message(data) {
+    const dataToString = data.toString();
+    dataFromUser = JSON.parse(dataToString);
+
+    switchCommand(dataFromUser, ws);
+  });
+
+  ws.on('close', function close() {
+    console.log('disconnected');
+  });
+});
