1ec1f6c03517fe8da912888cbfae2afb726053b2
Marigza <marigola@mail.ru>
2024-02-26 11:30:38 +0300

    refactor: update helper functions

--- src/battle_ship/helpers.ts
+++ src/battle_ship/helpers.ts
@@ -1,4 +1,4 @@
-import { CurrentSessionUser } from './models';
+import { CurrentSessionUser, Ship, ShipInBattle } from './models';
 
 export function checkExistingUser(
   userData: string,
@@ -38,4 +38,52 @@ export function generateCoord(array: string[]) {
   } while (array.includes(coords));
 
   return { x: randomX, y: randomY };
+}
+
+export function markShipsPosition(shipsFromUser: Ship[]) {
+  const shipsLocation: ShipInBattle[] = [];
+
+  let shipElement;
+  shipsFromUser.forEach((ship) => {
+    if (ship.length === 1) {
+      shipElement = JSON.stringify({ x: ship.position.x, y: ship.position.y });
+      const shipPos = [shipElement];
+      shipsLocation.push({
+        position: shipPos,
+        length: ship.length,
+        type: ship.type,
+        isKill: false,
+        isShot: false,
+        aliveLength: ship.length,
+      });
+    } else {
+      const shipPos = [];
+      for (let i = 0; i < ship.length; i++) {
+        let shipElement: string;
+
+        if (ship.direction === true) {
+          shipElement = JSON.stringify({
+            x: ship.position.x,
+            y: ship.position.y + i,
+          });
+          shipPos.push(shipElement);
+        } else if (ship.direction === false) {
+          shipElement = JSON.stringify({
+            x: ship.position.x + i,
+            y: ship.position.y,
+          });
+          shipPos.push(shipElement);
+        }
+      }
+      shipsLocation.push({
+        position: shipPos,
+        length: ship.length,
+        type: ship.type,
+        isKill: false,
+        isShot: false,
+        aliveLength: ship.length,
+      });
+    }
+  });
+  return shipsLocation;
 }
